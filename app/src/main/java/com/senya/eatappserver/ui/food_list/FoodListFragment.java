aae038a3ace64bb43a6a0b2cd4fcafa028d16dae
sen9ka <ars-shvec@yandex.ru>
2022-04-28 00:06:02 +0300

    Удалить айтем еды

--- app/src/main/java/com/senya/eatappserver/ui/food_list/FoodListFragment.java
+++ app/src/main/java/com/senya/eatappserver/ui/food_list/FoodListFragment.java
@@ -1,5 +1,6 @@
 package com.senya.eatappserver.ui.food_list;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,8 +8,10 @@ import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
@@ -17,13 +20,20 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
 import com.senya.eatappserver.R;
 import com.senya.eatappserver.adapter.MyFoodListAdapter;
 import com.senya.eatappserver.common.Common;
+import com.senya.eatappserver.common.MySwipeHelper;
 import com.senya.eatappserver.databinding.FragmentFoodListBinding;
 import com.senya.eatappserver.model.FoodModel;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,6 +42,8 @@ import butterknife.Unbinder;
 public class FoodListFragment extends Fragment {
 
     private FoodListViewModel foodListViewModel;
+
+    private List<FoodModel> foodModelList;
 
     Unbinder unbinder;
     @BindView(R.id.recycler_food_list)
@@ -49,9 +61,12 @@ public class FoodListFragment extends Fragment {
         unbinder = ButterKnife.bind(this,root);
         initViews();
         foodListViewModel.getMutableLiveDataFoodList().observe(getViewLifecycleOwner(), (Observer<List<FoodModel>>) foodModels -> {
-            adapter = new MyFoodListAdapter(getContext(),foodModels);
-            recycler_food_list.setAdapter(adapter);
-            recycler_food_list.setLayoutAnimation(layoutAnimationController);
+            if(foodModels != null) {
+                foodModelList = foodModels;
+                adapter = new MyFoodListAdapter(getContext(), foodModelList);
+                recycler_food_list.setAdapter(adapter);
+                recycler_food_list.setLayoutAnimation(layoutAnimationController);
+            }
         });
 
         return root;
@@ -67,5 +82,58 @@ public class FoodListFragment extends Fragment {
 
         layoutAnimationController = AnimationUtils.loadLayoutAnimation(getContext(),R.anim.layout_item_from_left);
 
+        MySwipeHelper mySwipeHelper = new MySwipeHelper(getContext(),recycler_food_list,300) {
+            @Override
+            public void instantiateMyButton(RecyclerView.ViewHolder viewHolder, List<MyButton> buf) {
+                buf.add(new MyButton(getContext(),"Delete",30,0, Color.parseColor("#9b0000"),
+                        pos -> {
+
+                        if(foodModelList != null)
+                            Common.selectedFood = foodModelList.get(pos);
+                            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                            builder.setTitle("DELETE")
+                                    .setMessage("Are you sure you want to delete this item?")
+                                    .setNegativeButton("CANCEL", ((dialogInterface, i) -> dialogInterface.dismiss()))
+                                    .setPositiveButton("DELETE", ((dialogInterface, i) -> {
+
+                                        Common.categorySelected.getFoods().remove(pos);
+                                        updateFood(Common.categorySelected.getFoods());
+                                    }));
+                            AlertDialog deleteDialog = builder.create();
+                            deleteDialog.show();
+
+                        }));
+
+                buf.add(new MyButton(getContext(),"Update",30,0, Color.parseColor("#560027"),
+                        pos -> {
+
+
+
+                        }));
+            }
+        };
+    }
+
+    private void updateFood(List<FoodModel> foods) {
+        Map<String,Object>updateData = new HashMap<>();
+        updateData.put("foods",foods);
+
+        FirebaseDatabase.getInstance()
+                .getReference(Common.CATEGORY_REF)
+                .child(Common.categorySelected.getMenu_id())
+                .updateChildren(updateData)
+                .addOnFailureListener(e -> {
+                    Toast.makeText(getContext(), ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                })
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful())
+                        {
+                            foodListViewModel.getMutableLiveDataFoodList();
+                            Toast.makeText(getContext(), "Item was deleted", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
