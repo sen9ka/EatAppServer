d53128d1c74b6b75293faa3b4d172e5f64941d3f
sen9ka <ars-shvec@yandex.ru>
2022-05-06 22:55:57 +0300

    Просмотр заказов по категориям

--- app/src/main/java/com/senya/eatappserver/HomeActivity.java
+++ app/src/main/java/com/senya/eatappserver/HomeActivity.java
@@ -72,9 +72,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
+      return true;
     }
 
     @Override
--- app/src/main/java/com/senya/eatappserver/ui/order/OrderFragment.java
+++ app/src/main/java/com/senya/eatappserver/ui/order/OrderFragment.java
@@ -2,10 +2,14 @@ package com.senya.eatappserver.ui.order;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -16,6 +20,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.senya.eatappserver.R;
 import com.senya.eatappserver.adapter.MyOrderAdapter;
+import com.senya.eatappserver.common.BottomSheetOrderFragment;
+import com.senya.eatappserver.model.EventBus.ChangeMenuClick;
+import com.senya.eatappserver.model.EventBus.LoadOrderEvent;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,6 +35,8 @@ import butterknife.Unbinder;
 public class OrderFragment extends Fragment {
     @BindView(R.id.recycler_order)
     RecyclerView recycler_order;
+    @BindView(R.id.txt_order_filter)
+    TextView txt_order_filter;
 
     Unbinder unbinder;
     LayoutAnimationController layoutAnimationController;
@@ -47,15 +60,68 @@ public class OrderFragment extends Fragment {
                 adapter = new MyOrderAdapter(getContext(),orderModels);
                 recycler_order.setAdapter(adapter);
                 recycler_order.setLayoutAnimation(layoutAnimationController);
+
+                txt_order_filter.setText(new StringBuilder("Orders (")
+                .append(orderModels.size())
+                .append(")"));
             }
         });
         return root;
     }
 
     private void initViews() {
+
+        setHasOptionsMenu(true);
+
         recycler_order.setHasFixedSize(true);
         recycler_order.setLayoutManager(new LinearLayoutManager(getContext()));
 
         layoutAnimationController = AnimationUtils.loadLayoutAnimation(getContext(),R.anim.layout_item_from_left);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.order_filter_menu,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.action_filter:
+                BottomSheetOrderFragment bottomSheetOrderFragment = BottomSheetOrderFragment.getInstance();
+                bottomSheetOrderFragment.show(getActivity().getSupportFragmentManager(),"OrderFilter");
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(!EventBus.getDefault().isRegistered(this))
+            EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        if(EventBus.getDefault().hasSubscriberForEvent(LoadOrderEvent.class))
+            EventBus.getDefault().removeStickyEvent(LoadOrderEvent.class);
+        if(EventBus.getDefault().isRegistered(this))
+            EventBus.getDefault().unregister(this);
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        EventBus.getDefault().postSticky(new ChangeMenuClick(true));
+        super.onDestroy();
+    }
+
+    @Subscribe(sticky = true,threadMode = ThreadMode.MAIN)
+    public void onLoadOrderEvent(LoadOrderEvent event)
+    {
+        orderViewModel.loadOrderByStatus(event.getStatus());
     }
 }
